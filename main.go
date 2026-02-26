40fcddfc34f64016cb097fdf4b214a1eccddaf00
FabulousBernt <johnny.vigersten@icloud.com>
2026-02-26 08:27:15 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "log"
+
+    "github.com/fabulousbernt/gator/internal/config"
+)
+
+func main() {
+    // 1. Read the config file
+    cfg, err := config.Read()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // 2. Set the current user and write to disk
+    err = cfg.SetUser("Johnny")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // 3. Read it again and print
+    cfg, err = config.Read()
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(cfg)
+}
